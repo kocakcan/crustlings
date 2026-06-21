20d90e402afeb8d1d5da5fbe4cc7059ebe13dcee
cankocak <can_kocak1@epam.com>
2026-06-21 11:58:38 +0300

    learned more than book in 1 hour

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,7 @@
 fn main() {
     // You can optionally experiment here.
+    let x = [1, 2, 3, 4, 5];
+    println!("{:?}", &x[..x.len() - 1]);
 }
 
 #[cfg(test)]
@@ -10,6 +12,7 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
+        let nice_slice = &a[1..a.len() - 1];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
