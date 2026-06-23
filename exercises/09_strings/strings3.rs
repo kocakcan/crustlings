4557fce83c9711aed43fc3fc18e07708be863a5f
kocakcan <can_kocak1@epam.com>
2026-06-23 17:37:15 +0300

    format returns string

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,28 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
     // You can optionally experiment here.
+    let best_boss = String::from("  Knight Artorias   ");
+    let mut name = String::from("Can");
+    name.push_str(" Kocak");
+    let cars = String::from("cars");
+    let cars = replace_me(&cars);
+    println!("{}", best_boss.trim());
+    println!("{}", cars);
+    println!("{}", name);
 }
 
 #[cfg(test)]
