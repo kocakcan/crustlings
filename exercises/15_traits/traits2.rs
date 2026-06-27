37d523d13e5d809ba9753e9f7396f0dd20275b02
cankocak <can_kocak1@epam.com>
2026-06-27 16:29:12 +0300

    i tried adding Bar to every string

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -4,9 +4,30 @@ trait AppendBar {
 
 // TODO: Implement the trait `AppendBar` for a vector of strings.
 // `append_bar` should push the string "Bar" into the vector.
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
+    let kocaks: Vec<String> = vec![
+        String::from("Can"),
+        String::from("Seyfi"),
+        String::from("Leyli"),
+        String::from("Dilan"),
+        String::from("Medet"),
+    ];
+    // println!(
+    //     "{:?}",
+    //     kocaks
+    //         .iter()
+    //         .map(|k| format!("{k}Bar"))
+    //         .collect::<Vec<String>>()
+    // );
+    println!("{:?}", kocaks.append_bar());
 }
 
 #[cfg(test)]
